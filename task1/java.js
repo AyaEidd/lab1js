7d60262a54b6082d6edf4b327145abc855720c5e
Aya Eid ♥ <ayaeid783@yahoo.com>
2022-11-30 21:13:59 +0200

    upload1

---
+++ task1/java.js
@@ -0,0 +1,9 @@
+var first =prompt("Enter your first name");
+var last=prompt("Enter your last name") ;
+var full= first +" " +last ;
+var conf=confirm("your full name is"+" " + full);
+var year=prompt("Enter your birth year");
+var certainYear=2022;
+var agee= certainYear - year
+confirm("you are" +" "+agee)
+alert("welcome" +" " + full +" "+"your age is" +" "+ agee)
---
+++ task2/task2.js
@@ -0,0 +1,6 @@
+alert("It is the first release of a calculator that only has a summation feature");
+var num1 =parseInt(prompt("Enter the first number"));
+var num2 =parseInt(prompt("Enter the second number"));
+var sum =  num1 + num2;
+alert(`The sum of ${num1} and ${num2} is ${sum}`);
+
