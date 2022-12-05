0c3ddeacf1ac77be4b00c45f8533855eeedec074
Aya Eid ♥ <ayaeid783@yahoo.com>
2022-12-05 09:09:57 +0200

    lab2java

---
+++ lab2/form.js
@@ -0,0 +1,98 @@
+// var user=document.getElementById('inuse').value ;
+// var psword=document.getElementById('inpass').value ;
+
+// function clickk(){
+//     if (user="admin", psword=123) {
+//         console.log('welcome');
+//       }
+//     else{
+//         console.log('not registered');
+//     }
+// }
+// var logg=document.getElementById('loginbtn') ;
+
+
+// logg.onsubmit=alert('hello') ;
+// console.log(logg) ;
+// logg.onsubmit = function(){
+//     alert('hello')
+// }
+
+// const logg=document.getElementById('loginbtn') ;
+
+// function clickko(user,psword){
+//     if (user=='admin', psword=='123') {
+//         alert('welcome');
+//         }
+//     else{
+//         alert('not registered');
+//     }
+// }
+// const logg=document.getElementById("loginbtn") ;
+// logg.addEventListener("click"), (e) => {
+// //     e.preventDefault();
+//     const fd =new FormData(tryform) ;
+//     const obj=Object.fromEntries(fd) ;
+    
+// // }
+
+// function clickoo(){
+//     if((user=='admin') && (psword==123)){
+
+//     // if(obj[0]=='admin'&&obj[1]==123){
+//         alert("welcome") ;
+//     }
+//     else{
+//         alert("not registered") ;
+//     }
+// }
+// logg.addEventListener("click",clickoo );
+
+
+
+//     var user=document.getElementById('inuse').value ;
+//     var psword=document.getElementById('inpass').value ;
+//     var logg=document.getElementById('loginbtn') ;
+    
+// function cli(){
+//     if ((user=='admin')&&(psword=='123')) {
+//         alert('welcome');
+//         }
+//     else{
+//         alert('not registered');
+//     }
+// }
+// logg.onclick=cli;
+
+// var user=document.getElementById('inuse').value ;
+// var psword=document.getElementById('inpass').value ;
+
+
+function myfun(){
+    var user = document.getElementById("userna").value ;
+    var pswd = document.getElementById("userpass").value ;
+    if(user == "admin" && pswd == "123") {
+        alert("welcome") ;
+    }
+    else{
+        alert("not registered") ;
+    }
+}
+
+
+//     var text;
+//     var user=document.getElementById('inuse').value ;
+//     var psword=document.getElementById('inpass').value ;
+//     var logg=document.getElementById('loginbtn') ;
+    
+// function cli(){
+//     if ((user='admin')&&(psword=123)) {
+//         text='welcome';
+//         }
+//     else{
+//         text='not registered';
+//     }
+// Document.getElementById("loginbtn").innertHTML =text ;
+
+// } 
+
